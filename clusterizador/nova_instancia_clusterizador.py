206cccffc4795f5f9ccd62d7a63322211d78e775
Y4nnLS <yann_lucas04@hotmail.com>
2024-05-20 16:27:01 -0300

    Update nova_instancia_clusterizador.py

--- clusterizador/nova_instancia_clusterizador.py
+++ clusterizador/nova_instancia_clusterizador.py
@@ -16,10 +16,10 @@ df_teste = pd.DataFrame([teste_instancia], columns=['Nitrogen', 'Phosphorus', 'P
 
 # Normalizar os dados da nova instância
 dados_numericos_normalizados = normalizador.transform(df_teste)
-dados_numericos_normalizados = pd.DataFrame(data=dados_numericos_normalizados, columns=['Nitrogen', 'Phosphorus', 'Potassium', 'Temperature', 'Humidity', 'pH_Value', 'Rainfall'])
+dados_numericos_normalizados_df = pd.DataFrame(data=dados_numericos_normalizados, columns=['Nitrogen', 'Phosphorus', 'Potassium', 'Temperature', 'Humidity', 'pH_Value', 'Rainfall'])
 
 # Predizer o grupo da nova instância e a centroide correspondente
-grupo_predito = crop_kmeans_model.predict(dados_numericos_normalizados)
+grupo_predito = crop_kmeans_model.predict(dados_numericos_normalizados_df)
 centroide = crop_kmeans_model.cluster_centers_[grupo_predito]
 
 print(f"Índice do grupo da nova instância: {grupo_predito[0]}")
@@ -27,8 +27,11 @@ print(f"Centroide da nova instância: {centroide}")
 
 # Desnormalizar dados numéricos para legibilidade
 dados_normalizados_final_legiveis = normalizador.inverse_transform(dados_numericos_normalizados)
-dados_normalizados_final_legiveis = pd.DataFrame(data=dados_normalizados_final_legiveis, columns=['Nitrogen', 'Phosphorus', 'Potassium', 'Temperature', 'Humidity', 'pH_Value', 'Rainfall'])
+dados_normalizados_final_legiveis_df = pd.DataFrame(data=dados_normalizados_final_legiveis, columns=['Nitrogen', 'Phosphorus', 'Potassium', 'Temperature', 'Humidity', 'pH_Value', 'Rainfall'])
 
 # Exibir o DataFrame final com os dados legíveis
 print("Dados da nova instância (desnormalizados):")
-print(dados_normalizados_final_legiveis.to_string(index=False))
+print(dados_normalizados_final_legiveis_df.to_string(index=False))
+
+print("Dados da nova instância (normalizados):")
+print(dados_numericos_normalizados_df.to_string(index=False))
